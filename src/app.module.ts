0665f1ba0d7491011e2e1b445bbb208da46ecdab
Raphael <raphael@wrathcodes.com>
2023-09-25 16:30:42 -0300

    fixed prisma connection and created first controller: (CreateAccountController)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CreateAccountController } from './controllers/create-account.controller'
+import { Module } from '@nestjs/common'
+import { PrismaService } from './prisma/prisma.service'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [CreateAccountController],
+  providers: [PrismaService],
 })
 export class AppModule {}
---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,38 @@
+import {
+  Body,
+  ConflictException,
+  Controller,
+  HttpCode,
+  Post,
+} from '@nestjs/common'
+
+import { PrismaService } from 'src/prisma/prisma.service'
+
+@Controller('/accounts')
+export class CreateAccountController {
+  constructor(private prisma: PrismaService) {}
+
+  @Post()
+  @HttpCode(201)
+  async handle(@Body() body) {
+    const { name, email, password } = body
+
+    const userWithSameEmail = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (userWithSameEmail) {
+      throw new ConflictException('User with same email already exists!')
+    }
+
+    await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password,
+      },
+    })
+  }
+}
