c2228d8cafcab6e3e3f5ed13f82be2aa4138e8d6
Raphael <raphael@wrathcodes.com>
2024-01-12 12:50:26 -0300

    feat: setup e2e testing database autodelete

--- test/setup-e2e.ts
+++ test/setup-e2e.ts
@@ -1,13 +1,33 @@
 import 'dotenv/config'
 
 import { PrismaClient } from '@prisma/client'
+import { execSync } from 'node:child_process'
+import { randomUUID } from 'crypto'
 
 const prisma = new PrismaClient()
 
+function generateUniqueDatabaseURL(schemaId: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable.')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schemaId)
+
+  return url.toString()
+}
+
+const schemaId = randomUUID()
 beforeAll(async () => {
-  console.log('beforeAll')
+  const databaseURL = generateUniqueDatabaseURL(schemaId)
+
+  process.env.DATABASE_URL = databaseURL
+
+  execSync('pnpm prisma migrate deploy')
 })
 
 afterAll(async () => {
-  console.log('afterAll')
+  await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schemaId}" CASCADE;`)
+  await prisma.$disconnect()
 })
